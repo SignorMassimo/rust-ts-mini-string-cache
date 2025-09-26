abb6a95c96ffbcd32886763702a08dcc196a4942
Z3XS0N <pervanisgenderli4@gmail.com>
2025-09-26 10:20:27 +0400

    u
    u

--- main.js
+++ main.js
@@ -2,8 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = require("./index");
 const cache = new index_1.MiniCache();
-cache.add('token', 'token value');
+//! add - get
+cache.add('token', 'user-token');
 console.log(cache.get('token'));
-cache.set('token', 'KLCJBREKCMAJSFDVKREIVKLSDJBVJERV');
-console.log(cache.get('token'));
+//! delete - has
+// console.log(cache.has('token'))
+// cache.delete('token')
+// console.log(cache.has('token'))
+//! filter - all
 console.log(cache.all());
+cache.add('env', '.env');
+console.log(cache.filter(['token']));
+//! set
+cache.set('token', 'token user');
+console.log(cache.get('token'));
